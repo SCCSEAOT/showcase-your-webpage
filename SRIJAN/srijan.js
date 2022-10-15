cfadbc384517cff59eef0c3f22ede3194e2191d1
Srijan Mukherjee <100375674+srijan-404@users.noreply.github.com>
2022-10-15 11:37:20 +0530

    Create srijan.js

---
+++ SRIJAN/srijan.js
@@ -0,0 +1,15 @@
+
+
+$('.nav li').click(function(){
+    $('.nav li').removeClass('active');
+    $(this).addClass('active');
+})
+
+$(".intro-header").css("height", $(window).height());
+
+$(".about-page").css("height", $(window).height());
+
+$(".portfolio-page").css("height", $(window).height());
+
+$(".contact-page").css("height", $(window).height());
+
