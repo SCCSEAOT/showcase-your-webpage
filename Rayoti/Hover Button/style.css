e40751ebe73bd75362a387f609c149fb6dfe16bd
Rayoti Kar <89602362+RayotiKar@users.noreply.github.com>
2022-10-23 14:51:49 +0530

    Create style.css

---
+++ Rayoti/Hover Button/style.css
@@ -0,0 +1,31 @@
+* {
+    box-sizing: border-box;
+  }
+  
+  body{
+    background-color: #111;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    overflow: hidden;
+    margin: 0;
+  }
+  .container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+    max-width: 1800px;
+  }
+  .square{
+    background-color: #ldldld;
+    box-shadow: 0 0 2px #000;
+    height: 16px;
+    width: 16px;
+    margin: 2px;
+    transition: 1s ease;
+  }
+  .square:hover {
+    transition-duration: 0.5s;
+  }
